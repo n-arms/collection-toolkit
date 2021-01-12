cec52c623fb8e90311ac2ab4ea158bd613dd58e9
n-arms <armstrong@Armstrongs-MacBook.local>
2021-01-12 12:10:08 -0500

    Implement hash_string with a rolling poly hash

--- include/hashmap.hpp
+++ include/hashmap.hpp
@@ -1,9 +1,18 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 #include "hashmap_declaration.hpp"
+#include "linkedlist.hpp"
+#include <string>
+#include <cmath>
 
-unsigned long long hash_string(const string& s){
-  return 1ULL;
+unsigned long long toolkit::hash_string(const std::string& s){
+  unsigned long long total = 0;
+  short p = 31;
+  unsigned long long m = 0x3B9ACA09;
+  for (int i = 0; i<s.size(); i++){
+    total += int(s[i])*std::pow(p, i);
+  }
+  return total%m;
 }
 
 
